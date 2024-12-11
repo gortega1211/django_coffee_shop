21049d24bcdf2081b43b583c8a8e8ee1d3e23102
Gustavo Ortega <gustavo.ortega.palacios@gmail.com>
2024-12-11 01:27:21 -0500

    [IMP] Vistas para formilario y listado de productos

--- products/views.py
+++ products/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import ListView, FormView
 
-# Create your views here.
+from .models import Product
+from .forms import ProductForm
+
+class ProductListView(ListView):
+    model = Product
+    template_name = "product_list.html"
+    context_object_name = "products"
+
+class ProductFormView(FormView):
+    template_name="product_add.html"
+    form_class = ProductForm
+    success_url = reverse_lazy("product_list")
+
+    def form_valid(self, form):
+        form.save()
+        return super().form_valid(form)
