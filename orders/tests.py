e7002ad343e283d539b6ff0998abd881b75aa034
Gustavo Ortega <gustavo.ortega.palacios@gmail.com>
2025-01-01 22:18:09 -0500

    [UPD] Agregandos pruebas unitarias  productos y ordenes

--- orders/tests.py
+++ orders/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+from django.contrib.auth import get_user_model
+
+class MyOrderViewTests(TestCase):
+
+    def test_no_logged_user_should_redirect(self):
+        url = reverse('my_order')
+        response = self.client.get(url)
+        # breakpoint()
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response.url, '/users/login/?next=/orders/my-order')
+
+    def test_logged_user_should_redirect(self):
+        url = reverse('my_order')
+        user = get_user_model().objects.create(username="test")
+        self.client.force_login(user)
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
